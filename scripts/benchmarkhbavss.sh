f0d27232f1b4d32717d36e68a00834aba8b5da83
ll6 <ll6@illinois.edu>
2020-08-31 12:11:55 -0500

    Change benchmark scripts.

--- scripts/benchmarkhbavss.sh
+++ scripts/benchmarkhbavss.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pytest --benchmark-save=hbavss_dummy_pcl --benchmark-min-rounds=3 --benchmark-warmup-iterations=0 benchmark/test_benchmark_hbavss_loglin.py
 pytest --benchmark-save=pcl_detailed --benchmark-min-rounds=3 --benchmark-warmup-iterations=0 benchmark/test_benchmark_poly_commit_log.py
+pytest --benchmark-save=hbavss_dummy_pcl --benchmark-min-rounds=3 --benchmark-warmup-iterations=0 benchmark/test_benchmark_hbavss_loglin.py
 pytest --benchmark-save=hbavss_actual_pcl --benchmark-min-rounds=3 --benchmark-warmup-iterations=0 benchmark/test_benchmark_hbavss_actual_loglin.py
